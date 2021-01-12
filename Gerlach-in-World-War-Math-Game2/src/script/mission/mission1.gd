cc601c500b0736166a97b6ec52451fd1b7a7ed2c
Thanakorn <60030041@lmitl.ac.th>
2021-01-12 13:26:20 +0700

    mission

---
+++ Gerlach-in-World-War-Math-Game2/src/script/mission/mission1.gd
@@ -0,0 +1,13 @@
+extends Node2D
+func _ready():
+	position.x = 438.103
+	position.y = 455.701
+func _process(delta):
+	pass
+
+
+
+
+func _on_Area2D_body_exited(body):
+	print("T")
+	pass # Replace with function body.
