b6fa1f3fdc10147683ddc669841c6c7ce5481723
Thanakorn <60030041@lmitl.ac.th>
2021-01-12 16:15:30 +0700

    comment

--- Gerlach-in-World-War-Math-Game2/src/script/chapter1.gd
+++ Gerlach-in-World-War-Math-Game2/src/script/chapter1.gd
@@ -8,6 +8,7 @@ func _ready():
 	$HUD.position.y = hud.y
 	var inst_scene3 = load("res://src/scene/scene3.tscn").instance()
 	#add_child(inst_scene3)
+	Events.emit_signal("HUD",true)
 	pass
 
 func _process(delta):
@@ -15,6 +16,7 @@ func _process(delta):
 	pass
 
 func player_position(position):
+	#ย่าย HUD ตามผู้เล่น
 	$HUD.position.x = position.x + hud.x
 	$HUD.position.y = position.y + hud.y
 	pass
